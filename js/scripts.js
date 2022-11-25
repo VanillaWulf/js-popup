1191cddb1a21a9eb73f50a1ca8f717d488090b61
Anna <annarsve@gmail.com>
2022-11-25 16:39:49 +0500

    add list

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,41 @@
 console.log('test');
 
-var value = 'value';
+var elOne = {
+    img: 'http://placekitten.com/300/300',
+    name: 'cat-1',
+    desc: 'cat -1 Lorem elitunem maxime nostrum nulla.'
+};
 
-console.log(value);
+var elTwo = {
+    img: 'http://placekitten.com/400/400',
+    name: 'cat-2',
+    desc: 'cat -2 Lorem elitunem maxime nostrum nulla.'
+};
 
-var button = document.getElementById('button-id');
+var elTree = {
+    img: 'http://placekitten.com/500/500',
+    name: 'cat-3',
+    desc: 'cat -3 Lorem elitunem maxime nostrum nulla.'
+};
 
-var popup = document.getElementById('popup-id');
+var list = document.getElementById('list');
 
-console.log(button, popup);
+function drawCat(cat){
+    var itemCat = document.createElement('li');
+    var content = '';
+    content = content + `<img src="${cat.img}" class="item-img">`;
+    content = content + `<h3>${cat.name}</h3>`;
+    content = content + `<p>${cat.desc}</p>`;
 
-console.log(button.textContent);
+    itemCat.classList.add('item');
+    itemCat.innerHTML = content;
 
-button.addEventListener('click', function () {
-    popup.classList.toggle('hide')
-});
+    list.appendChild(itemCat);
+}
+
+array = [elOne, elTwo, elTree];
+
+array.forEach((item) => drawCat(item));
+
+
+
